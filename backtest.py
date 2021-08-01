c057c5101143536cc73a84191656ad728063b450
kimshape <elves9918@gmail.com>
2021-08-01 13:50:50 +0900

    first commit

--- backtest.py
+++ backtest.py
@@ -1,16 +1,24 @@
 import pyupbit
 import numpy as np
 #OHLCV란, (open,high,low,close,volume) 당일 시가, 고가, 저가, 종가, 거래량에 대한 데이터
-df = pyupbit.get_ohlcv("KRW-BTC", count=7) # 7일 동안의 원화 BTC 시장에 대한 OHLCV
-df['range'] = (df['high'] - df['low']) * 0.5
+df = pyupbit.get_ohlcv("KRW-ETH", count=7) # 7일 동안의 원화 BTC 시장에 대한 OHLCV
+
+#변동폭 * k 계산, (고가-저가) * k값
+df['range'] = (df['high'] - df['low']) * 0.1
+
+#target(매수가) , range 컬럼을 한칸씩 밑으로 내림(.shift(1))
 df['target'] = df['open'] + df['range'].shift(1)
 
-
+#ror(수익률), np.where(numpy : 조건문, 참일때 값, 거짓일때 값)
 df['ror'] = np.where(df['high'] > df['target'],
                      df['close'] / df['target'],
                      1)
-
+#누적 곱 계산(cumprod) = > 누적 수익률
 df['hpr'] = df['ror'].cumprod()
+
+
 df['dd'] = (df['hpr'].cummax() - df['hpr']) / df['hpr'].cummax() * 100
 print("MDD(%): ", df['dd'].max())
 df.to_excel("dd.xlsx")
+
+print(df)
--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 import pyupbit
 
-access = "MJryrFhH6u9Luwc1ORGpVaJOB7Cjzr86gxpmx4ph"       
-secret = "uyuILZvdhRxexuAOjtrZl2YYN8UWMj9dBUGKLPtx"          # 본인 값으로 변경
+access = "ImZ2I3X2hpmhnc9tf841gghoOtjUug1gAH0hrwUO"       
+secret = "xZDwEvT5AAgGTHBr7K4XhVtl4cfEReHIZuAom9aX"          # 본인 값으로 변경
 upbit = pyupbit.Upbit(access, secret)
 
 print(upbit.get_balance("KRW-GAS"))     # KRW-XRP 조회
